db2d3e880adcb2083bdfe6adec7371fa43547662
Vincent Kyi <vincekyi@gmail.com>
2015-05-22 12:56:56 -0700

    Added Core package; defined basic query-service interface

--- src/main/java/org/copakb/server/core/QueryService.java
+++ src/main/java/org/copakb/server/core/QueryService.java
@@ -3,5 +3,13 @@ package org.copakb.server.core;
 /**
  * Created by vincekyi on 5/20/15.
  */
-public class QueryService {
+public interface QueryService {
+    //Todo return types
+
+    //Basic query functionality
+    void queryProtein(String uniprotID);
+    void queryProteinList(String[] uniprotIDs);
+    void queryPeptide(String sequence);
+    void queryDisease(String disease);
+    void retrieveReport(String token);
 }
