7ebef72356234b05b22df8f5cd2fbf116d232409
Alan Kha <akhahaha@gmail.com>
2016-01-15 03:26:23 -0800

    Cleanup StatisticsDAO

--- src/main/java/org/copakb/server/dao/StatisticsDAOImpl.java
+++ src/main/java/org/copakb/server/dao/StatisticsDAOImpl.java
@@ -1,6 +1,5 @@
 package org.copakb.server.dao;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.copakb.server.dao.model.ModuleStatistics;
 import org.hibernate.HibernateException;
 import org.hibernate.SQLQuery;
@@ -12,11 +11,13 @@ import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
 
+/**
+ * StatisticsDAO implementation.
+ */
 @SuppressWarnings("unchecked")
 public class StatisticsDAOImpl extends DAOImpl implements StatisticsDAO {
-
+    @Override
     public ModuleStatistics getModuleStatistics(int mod_id) throws HibernateException {
-
         update();
         Session session = sessionFactory.openSession();
 
@@ -26,17 +27,14 @@ public class StatisticsDAOImpl extends DAOImpl implements StatisticsDAO {
         return moduleStatistics;
     }
 
+    @Override
     public List<ModuleStatistics> list() {
         Session session = sessionFactory.openSession();
         List<ModuleStatistics> list = session.createCriteria(ModuleStatistics.class).list();
 
         session.close();
         return list;
-
-
     }
-
-
 
     private void update() {
         Session session = sessionFactory.openSession();
@@ -46,41 +44,33 @@ public class StatisticsDAOImpl extends DAOImpl implements StatisticsDAO {
         Calendar last = Calendar.getInstance();
         last.setTime(moduleStatistics.getLast_modified());
 
-
         Calendar current = Calendar.getInstance();
-//        System.out.println(current.get(current.DAY_OF_WEEK));
-//        System.out.println(last.get(current.DAY_OF_WEEK));
-//        System.out.println(current.get(current.DAY_OF_MONTH));
-//        System.out.println(last.get(current.DAY_OF_MONTH));
         if (current.get(current.DAY_OF_WEEK) == 1 && current.get(current.DAY_OF_MONTH) != last.get(last.DAY_OF_MONTH)) {
-
-
-            int[] mod_id_indexes = {1,2,3,4,5,6,8,9};
+            int[] mod_id_indexes = {1, 2, 3, 4, 5, 6, 8, 9};
             for (int i = 0; i < mod_id_indexes.length; i++) {
+                // Count number of proteins in each module
                 Transaction tx1 = session.beginTransaction();
-                String s = "SELECT COUNT(DISTINCT (protein_acc)) FROM COPADB.spectrum_protein WHERE mod_id = " + mod_id_indexes[i] +";";
+                String s = "SELECT COUNT(DISTINCT (protein_acc)) FROM COPADB.spectrum_protein WHERE mod_id = " + mod_id_indexes[i] + ";";
                 SQLQuery query = session.createSQLQuery(s);
                 BigInteger big = (BigInteger) query.list().get(0);
                 int nProteins = big.intValue();
-//                System.out.println(nProteins);
                 tx1.commit();
 
+                // Count number of peptides in each module
                 Transaction tx2 = session.beginTransaction();
                 s = "SELECT COUNT(DISTINCT (peptide_id)) FROM COPADB.spectrum WHERE mod_id = " + mod_id_indexes[i] + ";";
                 query = session.createSQLQuery(s);
                 big = (BigInteger) query.list().get(0);
                 int nPeptides = big.intValue();
-//                System.out.println(nPeptides);
                 tx2.commit();
 
+                // Count number of spectra in each module
                 Transaction tx3 = session.beginTransaction();
                 s = "SELECT COUNT(DISTINCT (spectrum_id)) FROM COPADB.spectrum WHERE mod_id = " + mod_id_indexes[i] + ";";
                 query = session.createSQLQuery(s);
                 big = (BigInteger) query.list().get(0);
                 int nSpectrum = big.intValue();
-//                System.out.println(nSpectrum);
                 tx3.commit();
-
 
                 Transaction tx = session.beginTransaction();
                 ModuleStatistics ms = (ModuleStatistics) session.get(ModuleStatistics.class, mod_id_indexes[i]);
@@ -91,12 +81,8 @@ public class StatisticsDAOImpl extends DAOImpl implements StatisticsDAO {
                 session.merge(ms);
                 tx.commit();
             }
-
         }
 
         session.close();
-
-        return;
     }
-
 }
